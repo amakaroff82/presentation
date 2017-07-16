541a3c7a5e2d6cb51f940efe8dda96c665575747
amakaroff82 <amakaroff82@gmail.com>
2017-07-16 21:45:52 +0300

    Small changes

--- js/main1.js
+++ js/main1.js
@@ -1,21 +1,16 @@
 //// init ////
 var scene = new THREE.Scene();									// create scene - container for objects(meshes)
 	
-var light = new THREE.PointLight(0xff5b9a, 1, 1000);			// create point light and add it on scene
-light.position.set( 5, 5, 5 );
+var light = new THREE.PointLight(0xFF9999, 1, 100);			// create point light and add it on scene
+light.position.set( 20, 20, 20 );
 scene.add( light );
 	
-var renderer = new THREE.WebGLRenderer();						// create renderer - by default canvas will be created 
-renderer.setSize( window.innerWidth, window.innerHeight );	
-document.body.appendChild( renderer.domElement );				// add canvas to body
-	
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );				// create simple box
-	
+var geometry = new THREE.BoxGeometry( 3, 3, 3 );				// create simple box
 var material = new THREE.MeshPhongMaterial( { 					// create material
-		color: 0x88ff77,
-		specular: 0x111111
+		color: 0x0000FF,
+		specular: 0xFFFFFF
 	} );
-	
+
 var cube = new THREE.Mesh( geometry, material );				// create mesh from geometry and material and add it on scene
 scene.add( cube );												
 	
@@ -25,16 +20,21 @@ var camera = new THREE.PerspectiveCamera( 						// create perspective camera
 		0.1, 
 		1000 
 	);
-	
 camera.position.z = 5;											// move camera back a bit
+
+	
+var renderer = new THREE.WebGLRenderer();						// create renderer - by default canvas will be created 
+renderer.setSize( window.innerWidth, window.innerHeight );	
+document.body.appendChild( renderer.domElement );				// add canvas to body
+	
 
 
 //// render ////	
 var animate = function () {										// render loop (idealy 60fps)
 	requestAnimationFrame( animate );
 	
-	cube.rotation.x += 0.01;									// transform object in local space
-	cube.rotation.y += 0.01;
+	cube.rotation.x += 0.003;									// transform object in local space
+	cube.rotation.y += 0.003;
 	
 	renderer.render(scene, camera);								// call render method per frame
 };
